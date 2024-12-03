80bf55241acecc97608085995a869db7d8f57b9c
toyobayashi <lifenglin314@outlook.com>
2024-12-03 11:21:16 +0800

    Set?

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
-{{{ ((DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.indexOf("$myLibraryInit") === -1 ? DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.push("$myLibraryInit") : undefined), "") }}}
-{{{ ((EXPORTED_RUNTIME_METHODS.indexOf("myLibraryInit") === -1 ? EXPORTED_RUNTIME_METHODS.push("myLibraryInit") : undefined), "") }}}
+{{{ ((DEFAULT_LIBRARY_FUNCS_TO_INCLUDE instanceof Set ? DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.add("$myLibraryInit") : DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.indexOf("$myLibraryInit") === -1 ? DEFAULT_LIBRARY_FUNCS_TO_INCLUDE.push("$myLibraryInit") : undefined), "") }}}
+{{{ ((EXPORTED_RUNTIME_METHODS instanceof Set ? EXPORTED_RUNTIME_METHODS.add("myLibraryInit") : EXPORTED_RUNTIME_METHODS.indexOf("myLibraryInit") === -1 ? EXPORTED_RUNTIME_METHODS.push("myLibraryInit") : undefined), "") }}}
 
 addToLibrary({
   $myLibraryInitOptions: {},
